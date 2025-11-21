9732c57cf2191622d7d8131f778f56cf4e164ea8
Saboor Ahmad <saboorahm7e@gmail.com>
2025-11-21 14:09:45 +0500

    Initial Commit

---
+++ TechHub/TechHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechHubApp.swift
+//  TechHub
+//
+//  Created by saboor on 21/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct TechHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
