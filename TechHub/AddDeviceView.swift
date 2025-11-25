e5c14d3d30b7794d29ba81408d371ce1f10bf722
Saboor Ahmad <saboorahm7e@gmail.com>
2025-11-25 14:56:19 +0500

    add icon, add device basic,search device

---
+++ TechHub/AddDeviceView.swift
@@ -0,0 +1,18 @@
+//
+//  AddDeviceView.swift
+//  TechHub
+//
+//  Created by saboor on 24/11/2025.
+//
+
+import SwiftUI
+
+struct AddDeviceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddDeviceView()
+}
